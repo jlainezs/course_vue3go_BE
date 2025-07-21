6420c15a3139e85859eb4ffb618e80fda9676c03
Pep Lainez <jlainezs@gmail.com>
2025-07-21 07:36:32 +0200

    8-98 Getting a list of all users from the back end API.

--- cmd/api/handlers.go
+++ cmd/api/handlers.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"net/http"
 	"time"
+	"wwwVuewgosrc/internal/data"
 )
 
 type jsonResponse struct {
@@ -102,4 +103,24 @@ func (app *application) Logout(w http.ResponseWriter, r *http.Request) {
 	}
 
 	_ = app.writeJSON(w, http.StatusOK, payload)
+}
+
+func (app *application) AllUsers(w http.ResponseWriter, r *http.Request) {
+	var users data.User
+	all, err := users.GetAll()
+	if err != nil {
+		app.errorLog.Println(err)
+		return
+	}
+
+	payload := jsonResponse{
+		Error:   false,
+		Message: "success",
+		Data:    envelope{"users": all},
+	}
+
+	err = app.writeJSON(w, http.StatusOK, payload)
+	if err != nil {
+		app.errorLog.Println(err)
+	}
 }
