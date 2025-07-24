197055585f7841e1436274c95029596031bd7fd8
Pep Lainez <jlainezs@gmail.com>
2025-07-24 22:18:46 +0200

    8-116 Checking status of token on every request
    Add handler and route for token validation in back end API.

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -26,6 +26,8 @@ func (app *application) routes() http.Handler {
 	mux.Post("/users/login", app.Login)
 	mux.Post("/users/logout", app.Logout)
 
+	mux.Post("/validate-token", app.ValidateToken)
+
 	mux.Route("/admin", func(mux chi.Router) {
 		mux.Use(app.AuthTokenMiddleware)
 		mux.Post("/users", app.AllUsers)
